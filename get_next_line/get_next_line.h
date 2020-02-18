3dd356f54fa83ce25c811f6d4e44da42b8d32811
root <yel-kobi@e2r6p8.1337.ma>
2020-02-18 12:36:25 +0100

    ff

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yel-kobi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/05 14:02:47 by yel-kobi          #+#    #+#             */
+/*   Updated: 2019/06/21 21:32:11 by yel-kobi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 32
+# define FD_NUM 4864
+
+# include "./libft/libft.h"
+# include <fcntl.h>
+
+int	get_next_line(const int fd, char **line);
+
+#endif
